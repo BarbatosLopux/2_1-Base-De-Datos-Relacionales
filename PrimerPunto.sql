6a20622a52562e4b7d7def1a410a690e96fd4f01
BarbatosLopux <sneideresteban@gmail.com>
2023-10-30 13:19:40 -0500

    feat: :sparkles: Creacion de tablas con entidades
    Se crearon las tablas pertinentes para el modelo de base de datos con sus entidades

---
+++ PrimerPunto.sql
@@ -0,0 +1,31 @@
+CREATE TABLE stock(
+    id_stock 
+);
+
+CREATE TABLE Libros(
+    id_libro 
+);
+CREATE TABLE Editores(
+    id_editor 
+
+);
+
+CREATE TABLE Almacenes(
+    id_almacen 
+);
+
+CREATE TABLE facturacion(
+    id_facturacion 
+
+);
+CREATE TABLE clientes(
+    id_clientes 
+
+);
+CREATE TABLE carrito(
+    shopping_cart_ID 
+);
+CREATE TABLE autor(
+    id_autor 
+
+);
